94669403a42a3a9748d45de9e3f8ac909f44d4e7
fpastana <felipeapastana@gmail.com>
2023-04-05 23:06:09 -0600

    Testing my first composer Library

---
+++ app/Http/Controllers/Tests/NumbersController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Tests;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Numbers;
+
+class NumbersController extends Controller
+{
+    public function index(Request $request)
+    {
+        if (isset($request->initialNumber) && isset($request->finalNumber)) {
+            $randomNumbers = new Numbers($request->initialNumber, $request->finalNumber);
+        } else {
+            $randomNumbers = new Numbers();
+        }
+
+
+        dd($randomNumbers->myShuffle());
+    }
+}
